b4af7e60814b387739a432a0686ea4bf3482f8d7
shvmpz05 <martinkimani914@gmail.com>
2022-10-16 18:03:39 +0000

    Only some alphabets

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char a;
+
+	for (a = 'a'; a < 'z'; a++)
+		if (a == 'e' || a == 'q')
+			continue;
+		else
+			putchar(a);
+	printf("\n");
+	return (0);
+}
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -2,3 +2,4 @@
 1- Last digit of values
 2- Printing the alphabets
 3- With capital alphabets
+4- Printing only some of the alphabets
