95311feccea382520d659d7920bd19ee0c5d36ba
shvmpz05 <martinkimani914@gmail.com>
2022-10-19 13:24:18 +0000

    Betty style

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -6,11 +6,11 @@
  *
  * @n: value to be checked
  *
- * Return: 0 or 1 
+ * Return: 0 or 1
  */
 
 int print_last_digit(int n)
 {
 	_putchar('0' + _abs(n % 10));
-	return (_abs( n % 10));
+	return (_abs(n % 10));
 }
