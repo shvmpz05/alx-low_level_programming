feab2968f9019b3269db17aec36608c146fbd46d
shvmpz05 <martinkimani914@gmail.com>
2022-10-21 12:29:28 +0000

    Checking if digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isdigit - Checks for numbers 0 to 9
+ * @c: parameter to pass 
+ * Return: 0
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -1,1 +1,2 @@
 0-Checking for uppercase
+1-Checking if digit
