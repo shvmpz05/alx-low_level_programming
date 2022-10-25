ba29262b7764882b6cc6904ec15dbc9ba8a53c91
shvmpz05 <martinkimani914@gmail.com>
2022-10-25 19:03:56 +0000

    Betty Style

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -11,11 +11,11 @@ void rev_string(char *s)
 {
 	int i;
 	char t;
-	
+
 	for (i = 0; i < _strlen(s) / 2; i++)
 	{
 		t = s[i];
 		s[i] = s[_strlen(s) - i - 1];
 		s[_strlen(s) - i - 1] = t;
-	}	
+	}
 }
