b75f7cd21bb0760a5aff5790a9f2159cddadb1ca
shvmpz05 <martinkimani914@gmail.com>
2022-10-25 16:12:48 +0000

    some stuff

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - gets the length of a string
+ *
+ * @s: pointer or address
+ *
+ * Return: length of string
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	}
+	return (i);
+}
--- 0x05-pointers_arrays_strings/README.md
+++ 0x05-pointers_arrays_strings/README.md
@@ -1,2 +1,3 @@
 0-Update variable using pointer
 1-Swap values with pointer
+3-Count length of a string
