f1fe270388ecc15292eca652353d3b39415b3004
shvmpz05 <113602416+shvmpz05@users.noreply.github.com>
2023-01-12 18:06:21 +0300

    some random stuff

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,10 +1,10 @@
 #include "lists.h"
 /**
- * print_dlistint - prints length and elements
+ * dlistint_len - length of linked list
  * @h: head of list
  * Return: size_t value
  */
-size_t print_dlistint(const dlistint_t *h)
+size_t dlistint_len(const dlistint_t *h)
 {
 	const dlistint_t *tmp;
 	int count = 0;
@@ -12,9 +12,8 @@ size_t print_dlistint(const dlistint_t *h)
 	tmp = h;
 	while (tmp != NULL)
 	{
-		printf("%d\n", tmp->n);
-		tmp = tmp->next;
 		count++;
+		tmp = tmp->next;
 	}
 	return (count);
 }
